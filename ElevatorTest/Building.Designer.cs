a11587cc2e98f5eb75f5c93dcf11d9a0fb1fb1b7
btkakashi <btkakashi@gmail.com>
2013-08-28 11:34:03 +0800

    add form

---
+++ ElevatorTest/Building.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ElevatorTest
+{
+    partial class Building
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.plBuilding = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.elevatorRunTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.plBuilding.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plBuilding
+            // 
+            this.plBuilding.BackColor = System.Drawing.Color.White;
+            this.plBuilding.Controls.Add(this.panel5);
+            this.plBuilding.Controls.Add(this.panel4);
+            this.plBuilding.Controls.Add(this.panel3);
+            this.plBuilding.Controls.Add(this.panel2);
+            this.plBuilding.Controls.Add(this.panel1);
+            this.plBuilding.Location = new System.Drawing.Point(174, 12);
+            this.plBuilding.Name = "plBuilding";
+            this.plBuilding.Size = new System.Drawing.Size(200, 532);
+            this.plBuilding.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 429);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 323);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(194, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 216);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(194, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(3, 110);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(194, 100);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(3, 4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(194, 100);
+            this.panel5.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStop.Location = new System.Drawing.Point(518, 538);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // Building
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 585);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.plBuilding);
+            this.Name = "Building";
+            this.Text = "Building";
+            this.plBuilding.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plBuilding;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer elevatorRunTimer;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
--- ElevatorTest/Program.cs
+++ ElevatorTest/Program.cs
@@ -3,46 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ElevatorTest
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    int maxFloor = 10;
+
+        //    List<People> peoples = new List<People>();
+        //    for (int i = 0; i < 10; i++)
+        //    {
+
+        //        Random ro = new Random(i);
+        //        int currentFloor = ro.Next(1, maxFloor);
+        //        int goFloor = ro.Next(1, maxFloor);
+        //        while (goFloor == currentFloor)
+        //        {
+        //            goFloor = ro.Next(1, maxFloor);
+        //        }
+
+        //        People p1 = new People(maxFloor);
+        //        p1.currentFloor = currentFloor;
+        //        p1.goFloor = goFloor;
+        //        p1.id = i;
+        //        peoples.Add(p1);
+
+        //        Utils.WriteMsg("People " + p1.id + " is on floor:" + p1.currentFloor + " and he go to floor:" + p1.goFloor);
+        //    }
+
+        //    Elevator e1 = new Elevator();
+        //    e1.maxFloor = maxFloor;
+        //    e1.capacity = 20;
+        //    e1.peoples = peoples;
+        //    e1.Run();
+
+        //    Console.ReadLine(); 
+        //}
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            int maxFloor = 10;
-
-            List<People> peoples = new List<People>();
-            for (int i = 0; i < 10; i++)
-            {
-
-                Random ro = new Random((int)DateTime.Now.Ticks);
-                int currentFloor = ro.Next(1, maxFloor);
-                int goFloor = ro.Next(1, maxFloor);
-                while (goFloor == currentFloor)
-                {
-                    goFloor = ro.Next(1, maxFloor);
-                }
-
-                People p1 = new People(maxFloor);
-                p1.currentFloor = currentFloor;
-                p1.goFloor = goFloor;
-                p1.id = i;
-                peoples.Add(p1);
-
-                Utils.WriteMsg("People " + p1.id + " is on floor:" + p1.currentFloor + " and he go to floor:" + p1.goFloor);
-            }
-
-            Elevator e1 = new Elevator();
-            e1.maxFloor = maxFloor;
-            e1.capacity = 20;
-            e1.peoples = peoples;
-            e1.Run();
-
-            while (true)
-            {
-                
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Building());
         }
     }
 }
